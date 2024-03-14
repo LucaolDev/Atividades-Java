7a3dc49111b718752a93f02c63f95bdabce0ed46
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-03-14 16:09:00 -0300

    Add files via upload

---
+++ Exercicios_material05/src/Exercicio06.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercicio06 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double x, y, r;
+	
+		
+		in.close();
+
+	}
+
+}
