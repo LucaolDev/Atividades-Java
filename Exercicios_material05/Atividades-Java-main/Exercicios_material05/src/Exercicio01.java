c2eb1b17f18bf7c2aa365814f02a88dfe5e418df
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-03-20 01:20:20 -0300

    Add files via upload

---
+++ Exercicios_material05/Atividades-Java-main/Exercicios_material05/src/Exercicio01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double base, altura, area, perimetro;
+		
+		//entrada de dados 
+		System.out.println("Digite a base");
+		base = in.nextDouble();
+		System.out.println("Digite a altura");
+		altura = in.nextDouble();
+		
+		//processamento
+		area = base * altura;
+		perimetro = 2 * (base + altura);
+		
+	    //saida
+		System.out.println("Area = " + area );
+		System.out.println("Perimetro = " + perimetro );
+		
+		in.close();
+	}
+
+}
