17a20818c3db4d394b7e66c5db0ba6d64d1e47fb
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-03-19 22:29:57 -0300

    Add files via upload

---
+++ Atividades-Java-main/Exercicios_material05/src/Exercicio05.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		double ValorDaHora, DescontoDoINSS, SB, SL, percentualINSS, HorasTrabalhadas;
+		
+        // entrada de dados
+		System.out.println("Informe o Valor da hora aula: "); 
+		ValorDaHora = in.nextDouble();
+		System.out.println("Informe o número de horas trabalhadas: ");
+		HorasTrabalhadas = in.nextInt();
+		System.out.println("Informe o a porcentagem de desconto do INSS: ");
+		percentualINSS = in.nextDouble();
+		
+		//processamento
+		SB = ValorDaHora * HorasTrabalhadas;
+		DescontoDoINSS = (SB * percentualINSS) / 100;
+		SL = SB - DescontoDoINSS;
+		
+		//saida
+		System.out.println("O salário líquido mensal é: " + String.format("%.2f" , SL));
+		
+        in.close();
+	}
+
+}
