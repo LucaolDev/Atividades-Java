feab64e334ac86dd84cb2f1a233f1a07163faea4
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-04-03 22:36:10 -0300

    Add files via upload

--- Exercicios_material05/src/Exercicio15.java
+++ Exercicios_material05/src/Exercicio15.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Exercicio15 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
 		double l1, l2, l3;
-		
+
 		System.out.print("Digite o valor do primeiro lado: ");
 		l1 = in.nextDouble();
 		System.out.print("Digite o valor do segundo lado: ");
@@ -14,23 +14,20 @@ public class Exercicio15 {
 		System.out.print("Digite o valor do terceiro lado: ");
 		l3 = in.nextDouble();
 		in.close();
-		
-        if (l1 < l2 + l3 && l2 < l1 + l3 && l3 < l1 + l2) {
-        	if (l1 == l2 && l2 == l3) {
-        	System.out.println("triangulo equilatero.");
-        	}
-        else {
-        	if (l1 == l2 || l1 == l3 || l2 == l3) {
-        		System.out.println("triangulo isosceles");
-        	}
-        	else {
-        		System.out.println("triangulo escaleno");
-        	}
-        }
-              
+
+		if (l1 < l2 + l3 && l2 < l1 + l3 && l3 < l1 + l2) {
+			if (l1 == l2 && l2 == l3) {
+				System.out.println("triangulo equilatero.");
+			} else {
+				if (l1 == l2 || l1 == l3 || l2 == l3) {
+					System.out.println("triangulo isosceles");
+				} else {
+					System.out.println("triangulo escaleno");
+				}
+			}
+
+		} else {
+			System.out.println("Os valores nao forma um triangulo");
+		}
 	}
-	else {
-		System.out.println("Os valores nao forma um triangulo");
-	}
- }
 }
--- Exercicios_material05/src/Exercicio16.java
+++ Exercicios_material05/src/Exercicio16.java
@@ -3,27 +3,29 @@ import java.util.Scanner;
 public class Exercicio16 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
-		double vd, dias;
-        
+		double vt;
+		int dias;
+
 		System.out.println("Digite quantos dias ficou no hotel: ");
-		dias = in.nextDouble();
+		dias = in.nextInt();
 		in.close();
-		
-		if (dias >= 16 ) {
-			vd = (250 * dias) + (15.50 * dias); 
-			System.out.println("Valor da conta: " + vd );
-		} else if (dias == 15) {
-			vd = (250 * dias) + (36 * dias); 
-			System.out.println("Valor da conta: " + vd );
-		}	else {
-				vd = (250 * dias) + (58 * dias); 
-				System.out.println("Valor da conta: " + vd );
+
+		if (dias > 0) {
+			if (dias >= 16) {
+				vt = dias * (250 + 15.50);
+			} else if (dias == 15) {
+				vt = dias * (250 + 36);
+			} else {
+				vt = dias * (250 + 58);
 			}
-				
-		
-		
+			System.out.println("Valor da conta: " + String.format("%.2f", vt));
+		} else {
+			System.err.println("Digite novamente");
+		}
+
 	}
 
 }
+// ctrl shift f 
