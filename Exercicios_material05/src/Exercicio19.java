5858cffd0f301ea7bfb1d8225ab64063d87a76c6
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-04-05 14:33:43 -0300

    Add files via upload

--- Exercicios_material05/src/Exercicio19.java
+++ Exercicios_material05/src/Exercicio19.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 
 public class Exercicio19 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int n1, n2, n3, temp;
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Digite os tres valores separados: ");
+		n1 = in.nextInt();
+		n2 = in.nextInt();
+		n3 = in.nextInt();
+		in.close();
+
+		if (n1 > n2) {
+			temp = n1;
+			n1 = n2;
+			n2 = temp;
+		}
+		if (n2 > n3) {
+			temp = n2;
+			n2 = n3;
+			n3 = temp;
+		}
+		if (n1 > n2) {
+			temp = n1;
+			n1 = n2;
+			n2 = temp;
+		}
+
+		System.out.println(n1 + ", " + n2 + ", " + n3);
 
 	}
 
