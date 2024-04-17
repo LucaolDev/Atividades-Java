cc5b7dd14610742f5f72aacc5a815be04c3ec283
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-04-17 13:08:42 -0300

    Add files via upload

--- Exercicios_material05/src/Exercicio20.java
+++ Exercicios_material05/src/Exercicio20.java
@@ -1,62 +1,79 @@
 import java.util.Scanner;
 
 public class Exercicio20 {
-
 	public static void main(String[] args) {
-
-		Scanner in = new Scanner(System.in);
-
-		System.out.println("Digite o sálario atual: ");
-		double salarioAtual = in.nextDouble();
-
-		System.out.println("Digite o cargo (Gerente, Engenheiro, Técnico): ");
-		String cargo = in.next();
-
-		System.out.println("Digite o tempo de serviço (anos):");
-		int tempoServico = in.nextInt();
+		Scanner in = new Scanner (System.in);
+		
+		String cargo;
+		int anos;
+		double salario, novosalario = 0, porcentagem = 0;
+		
+		System.out.println("Digite seu salário: ");
+		salario = in.nextDouble();
+		
+		System.out.println("Digite seu cargo: ");
+		cargo = in.next();
+		
+		System.out.println("Digite seu tempo de serviço em anos: ");
+		anos = in.nextInt();
 		in.close();
-
-		double aumento = calcularAumento(cargo, tempoServico);
-		double novoSalario = salarioAtual * (1 + aumento / 100);
-
-		System.out.println("Novo salário: " + novoSalario);
-		System.out.println("Salário antigo: " + salarioAtual);
-		System.out.println("Diferença: " + (novoSalario - salarioAtual));
-	}
-
-	public static double calcularAumento(String cargo, int tempoServico) {
-		double aumento;
-
-		switch (cargo) {
-		case "Gerente":
-			if (tempoServico >= 5)
-				aumento = 10;
-			else if (tempoServico >= 3)
-				aumento = 9;
+		
+		if(cargo.equalsIgnoreCase("gerente"))
+		{
+			if (anos >= 5)
+			{
+				porcentagem = 10;
+			}
+			else if (anos >= 3 && anos < 5)
+			{
+				porcentagem = 9;
+			}
 			else
-				aumento = 8;
-			break;
-		case "Engenheiro":
-			if (tempoServico >= 5)
-				aumento = 12;
-			else if (tempoServico >= 3)
-				aumento = 10;
-			else
-				aumento = 9;
-			break;
-		case "Técnico":
-			if (tempoServico >= 5)
-				aumento = 11;
-			else if (tempoServico >= 3)
-				aumento = 10;
-			else
-				aumento = 9;
-			break;
-		default:
-			aumento = 5;
+			{
+				porcentagem = 8;
+			}
 		}
-
-		return aumento;
+		
+		else if(cargo.equalsIgnoreCase("engenheiro"))
+		{
+			if (anos >= 5)
+			{
+				porcentagem = 11;
+			}
+			else if (anos >= 3 && anos < 5)
+			{
+				porcentagem = 10;
+			}
+			else
+			{
+				porcentagem = 9;
+			}
+		}
+		
+		else if(cargo.equalsIgnoreCase("técnico"))
+		{
+			if (anos >= 5)
+			{
+				porcentagem = 12;
+			}
+			if (anos >= 3 && anos < 5)
+			{
+				porcentagem = 11;
+			}
+			if (anos < 3)
+			{
+				porcentagem = 10;
+			}
+		}else {
+			porcentagem = 5;
+		}
+		
+		novosalario = salario + salario * porcentagem/100;
+		
+		System.out.println("Seu novo salário é: " + novosalario);
+		System.out.println("Seu salário antigo: " + salario);
+		System.out.println("Diferença de salário: " + (novosalario - salario));
+		
 	}
 
 }
